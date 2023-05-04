e16da322b2eae285a4df551894e1ee3e5df83531
Gaetan Moreau <gmoreau.contact@gmail.com>
2023-05-04 13:46:46 +0200

    add deletion of client

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -36,7 +36,7 @@ class ClientController extends AbstractController
             $client->setUser($this->getUser());
             $em->persist($client);
             $em->flush();
-            return $this->redirectToRoute('app_client   ');
+            return $this->redirectToRoute('app_client');
         }
 
         return $this->render('client/add.html.twig', [
@@ -44,7 +44,7 @@ class ClientController extends AbstractController
         ]);
     }
     #[Route('/client/{id}', name: 'app_client_id')]
-    public function edit(ClientRepository $cr, Request $request): Response
+    public function show(ClientRepository $cr, Request $request): Response
     {
         $clientId = $request->get('id');
         $client = $cr->find($clientId);
@@ -54,7 +54,18 @@ class ClientController extends AbstractController
         ]);
     }
     #[Route('/client/{id}/delete', name: 'app_client_delete')]
-    public function delete(ClientRepository $cr, Request $request): Response
+    public function delete(ClientRepository $cr, Request $request, EntityManagerInterface $em): Response
+    {
+        $clientId = $request->get('id');
+        $client = $cr->find($clientId);
+
+        $em->remove($client);
+        $em->flush();
+
+        return $this->redirectToRoute('app_client');
+    }
+    #[Route('/client/{id}/edit', name: 'app_client_edit')]
+    public function edit(ClientRepository $cr, Request $request, EntityManagerInterface $em): Response
     {
         $clientId = $request->get('id');
         $client = $cr->find($clientId);
