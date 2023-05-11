8e0f99064627b428f20b10656570c4a41d29ecff
Gaetan Moreau <gmoreau.contact@gmail.com>
2023-05-11 14:47:54 +0200

    improve style and components

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -15,8 +15,14 @@ class AccountController extends AbstractController
     #[Route('/', name: 'app_account')]
     public function index(): Response
     {
+        $user = $this->getUser();
+        $clients = $user->getClients();
+        $invoices = $user->getInvoices()    ;
+
         return $this->render('account/index.html.twig', [
             'controller_name' => 'AccountController',
+            'clients' => $clients,
+            'invoices' => $invoices
         ]);
     }
 }
